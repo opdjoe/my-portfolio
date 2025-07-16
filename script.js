4b5b623a115cf5ae36e5090af819e4a8f5335181
Opoku Duah <opokuduah19@gmail.com>
2025-07-15 23:57:59 -0400

    updates to add responsiveness and nav items menu toggler for different screen size;The hamburger menu effect on small and medium devices

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+
+// Function to toggle the responsive class on the top navigation bar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  // Using getElementsByClassName to select the first element with the class "fa fa
+  var icon = document.getElementsByClassName("fa fa-bars")[0];
+  // Toggle the icon class to change the icon when clicked
+  if (x.className === "topdiv") {
+    x.className += " responsive";
+    icon.classList.replace("fa-bars", "fa-close");
+    // console.log(icon.className)
+  } else {
+    x.className = "topdiv";
+    // Change the icon back to bars when the menu is closed
+    var icon = document.getElementsByClassName("fa fa-close")[0];
+    icon.classList.replace("fa-close", "fa-bars");
+    // console.log(icon.className)
+  }
+}
